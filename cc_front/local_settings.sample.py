acb1a40fca4d638f17b0665ab354ea63daf2df68
Hieu Nguyen <hieuh25@gmail.com>
2013-08-04 22:24:35 +0300

    Add Django 1.5

---
+++ cc_front/local_settings.sample.py
@@ -0,0 +1,19 @@
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'ibacb_d1jb)*)z9*$0coa=)!"s)/)(%!"kxq@(0hjq9z$cg-l3*z*4'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'cc.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# In a Windows environment this must be set to your system time zone.
+TIME_ZONE = 'Europe/Helsinki'
