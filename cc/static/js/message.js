43be7552e4102054adfc46b2134179709de56d62
Hieu Nguyen <hieuh25@gmail.com>
2013-09-27 00:40:19 +0300

    Toggle state of Send button with JS and add some JS validation to the form

--- cc/static/js/message.js
+++ cc/static/js/message.js
@@ -1,5 +1,65 @@
 $(document).ready(function () {
+    var message_form = $('#js-messageForm'),
+        message_submit_btn = $('#js-submitMessageForm');
 
+    // validation rules for message form
+    message_form.validate({
+        rules: {
+            subject: 'required',
+            to: 'required',
+            message: 'required',
+            attachment: 'required',
+        },
+        errorPlacement: function(error,element) {
+            return true;
+        },
+        submitHandler: function(form) {
+            form.submit();
+        }
+    });
+
+    // function to enable / disable send button
+    function toggleMessageSubmitButton(force_disable) {
+        if (typeof force_disable !== 'undefined' && force_disable) {
+            message_submit_btn.addClass('disabled');
+        }
+
+        if ($('#js-messageForm').valid()) {
+            message_submit_btn.removeClass('disabled');
+        } else {
+            message_submit_btn.addClass('disabled');
+        }
+    }
+
+    // bind validation on input keyup
+    message_form.find('input[type="text"], textarea').keyup(function () {
+        toggleMessageSubmitButton();
+    });
+
+    // submit the form when clicking Send button
+    message_submit_btn.click(function() {
+        if (!$(this).hasClass('disabled')) {
+            message_form.trigger('submit');
+        }
+        return false;
+    });
+
+    // use tokenfield for To field
+    $('#id_to').tokenfield({
+        minLength: 3
+    }).on('afterCreateToken', function (e) {
+        // validate the email
+        var re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        var valid = re.test(e.token.value);
+        if (!valid) {
+            $(e.relatedTarget).addClass('invalid');
+            toggleMessageSubmitButton(true);
+        }
+    }).on('removeToken', function (e) {
+        toggleMessageSubmitButton();
+    });
+
+    // dropzone config for file upload form
     Dropzone.options.uploadFileForm = {
         url: $(this).attr('action'),
         paramName: 'file',
@@ -19,33 +79,24 @@ $(document).ready(function () {
                 // remove error message 
                 $('#uploadFileForm .alert').remove();
 
-                // enable button
-                $('#js-submitMessageForm').removeClass('disabled');
+                toggleMessageSubmitButton();
             } else {
                 $('#uploadFileForm').prepend('<p class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button>' + t.ERROR_OCURRED_WITHOUT_DOT + ": " + response.message + '</p>');
+                console.log('Conversion error: ' + response.original_error);
 
                 $('.dz-error-mark').css('opacity', 1);
-                console.log('Conversion error: ' + response.original_error);
+                toggleMessageSubmitButton();
             }
         },
         error: function (file, errorMessage) {
             $('.dz-error-mark').css('opacity', 1);
             console.log(errorMessage);
+            toggleMessageSubmitButton();
         },
         maxfilesexceeded: function () {
             console.log('Max file exceed');
+            toggleMessageSubmitButton();
         },
     };
-
-    $('#js-submitMessageForm').click(function() {
-        if (!$(this).hasClass('disabled')) {
-            $('#js-messageForm').trigger('submit');
-        }
-        return false;
-    });
-
-    $('#id_to').tokenfield({
-        minLength: 3
-    });
 
 }); // end document ready
