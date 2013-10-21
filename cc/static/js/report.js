04292e801418c33517ce7942444f79cb63bcb7f6
Hieu Nguyen <hieuh25@gmail.com>
2013-10-21 15:28:33 +0300

    Ref #27: Init an example chart in report page

--- cc/static/js/report.js
+++ cc/static/js/report.js
@@ -1,5 +1,26 @@
 $(document).ready(function () {
+    // navigate with the dropdown
     $('#js_selectMessage').change(function () {
         window.location = '/report/' + $(this).val();
     });
+
+    // init the google charts
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Page', 'Total time'],
+            ['Page 1', 23],
+            ['Page 2', 25],
+            ['Page 3', 27],
+            ['Page 4', 3]
+        ]);
+
+        var options = {
+            title: 'Page graph',
+            hAxis: {title: 'Page', titleTextStyle: {color: 'red'}}
+        };
+
+        var chart = new google.visualization.ColumnChart(document.getElementById('report_graph'));
+        chart.draw(data, options);
+    }
+    google.load("visualization", "1", {packages:["corechart"], "callback" : drawChart});
 }); // end document ready
