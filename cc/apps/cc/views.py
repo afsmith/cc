d5e98d8abeecbe3e82f59bf4f25174fef7d772af
Hieu Nguyen <hieuh25@gmail.com>
2013-10-23 17:09:00 +0300

    Ref #27: Some real data for the graph

--- cc/apps/cc/views.py
+++ cc/apps/cc/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth import decorators as auth_decorators
 from django.shortcuts import redirect, get_object_or_404
 from django.db.models import Sum
+from django.http import HttpResponse
 
 from cc.apps.cc_messages.models import Message
 from cc.apps.tracking.models import TrackingSession, TrackingEvent
 
 from annoying.decorators import render_to
+import json
 
 
 @auth_decorators.login_required
@@ -37,26 +39,33 @@ def report_detail(request, message_id):
         .order_by('created_at').reverse()
     )
 
-    log = (
+    session_log = (
         TrackingSession.objects
         .filter(message=message_id)
         .extra(select={
             'total_time': "SELECT SUM(total_time) FROM tracking_trackingevent WHERE tracking_trackingevent.tracking_session_id = tracking_trackingsession.id"
         })
     )
-    
-    '''
-    TrackingEvent.objects.filter(tracking_session__message=message_id).aggregate(Sum('total_time'))
-    log2 = (
-        TrackingEvent.objects
-        .filter(tracking_session__message=message_id)
-        .values('tracking_session')
-        .annotate(total_time=Sum('total_time'))
-    )
-    '''
 
-    return {
-        'this_message': this_message,
-        'messages': all_messages,
-        'log': log,
-    }
+    # might be useful later
+    #TrackingEvent.objects.filter(tracking_session__message=message_id).aggregate(Sum('total_time'))
+
+    if request.is_ajax():
+        page_time_log = (
+            TrackingEvent.objects
+            .filter(tracking_session__message=message_id)
+            .values('page_number')
+            .annotate(total_time=Sum('total_time'))
+            .order_by('page_number')
+            .values_list('page_number', 'total_time') # convert it to flat list
+        )
+        to_second = [[x[0], x[1]/100.0] for x in list(page_time_log)]
+        print to_second
+        json_resp = json.dumps(to_second)
+        return HttpResponse(json_resp, mimetype='application/json')
+    else:
+        return {
+            'this_message': this_message,
+            'messages': all_messages,
+            'session_log': session_log,
+        }
--- cc/static/js/report.js
+++ cc/static/js/report.js
@@ -1,26 +1,50 @@
 $(document).ready(function () {
+    var drawChart;
+
     // navigate with the dropdown
     $('#js_selectMessage').change(function () {
         window.location = '/report/' + $(this).val();
     });
 
-    // init the google charts
-    function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-            ['Page', 'Total time'],
-            ['Page 1', 23],
-            ['Page 2', 25],
-            ['Page 3', 27],
-            ['Page 4', 3]
-        ]);
+    // draw google charts
+    drawChart = function () {
+        var chart_data,
+            data,
+            options,
+            chart;
 
-        var options = {
-            title: 'Page graph',
-            hAxis: {title: 'Page', titleTextStyle: {color: 'red'}}
-        };
+        chart_data = $.ajax({
+            url: window.location,
+            type: 'GET',
+            dataType: 'json',
+        }).done(function (resp) {
+            data = google.visualization.arrayToDataTable($.merge(
+                [['Page', 'Total time']],
+                resp
+            ));
 
-        var chart = new google.visualization.ColumnChart(document.getElementById('report_graph'));
-        chart.draw(data, options);
-    }
+            options = {
+                title: 'Page graph',
+                hAxis: {
+                    //title: 'Page',
+                    //titleTextStyle: {color: 'red'},
+                    format: 'Page #',
+                    gridlines: {
+                        count: resp.length
+                    }
+                },
+                height: 500,
+            };
+
+            chart = new google.visualization.ColumnChart(document.getElementById('report_graph'));
+            chart.draw(data, options);
+        });
+
+        
+    };
     google.load("visualization", "1", {packages:["corechart"], "callback" : drawChart});
+    
+    // get page total time and display
+    
+
 }); // end document ready
