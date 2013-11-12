6901d28b00e66fc8beba108a2a4d540d7d22dc83
Hieu Nguyen <hieuh25@gmail.com>
2013-11-13 01:35:53 +0200

    Resolve #51: user admin

---
+++ cc/apps/accounts/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+
+from . import models
+
+
+class UserTypeListFilter(admin.SimpleListFilter):
+    title = 'user type'
+    parameter_name = 'type'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('sender', 'Sender'),
+            ('recipient', 'Recipient'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'sender':
+            return queryset.exclude(country='N/A')
+        if self.value() == 'recipient':
+            return queryset.filter(country='N/A')
+
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('email', 'first_name', 'last_name')
+    list_display = (
+        'email', 'first_name', 'last_name',
+        'is_active', 'user_type', 'date_joined', 'last_login'
+    )
+    list_filter = (UserTypeListFilter, 'is_active')
+    ordering = ('-date_joined', 'last_login')
+
+    def user_type(self, obj):
+        if obj.country == 'N/A':
+            return 'Recipient'
+        else:
+            return 'Sender'
+
+admin.site.register(models.CUser, UserAdmin)
