419d35c3c057aa0d932ce51da2204a83ee51826c
Hieu Nguyen <ndhieu88@gmail.com>
2013-12-02 03:14:43 +0200

    Resolved #52: Modify message admin to support filter by date range

--- cc/apps/cc_messages/admin.py
+++ cc/apps/cc_messages/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
 
 from . import models
 
 
 class MessageAdmin(admin.ModelAdmin):
     search_fields = ('subject',)
-    list_display = ('subject', 'message', 'owner')
-    list_filter = ('owner', )
+    list_display = (
+        'subject', 'message', 'owner', 
+        'cc_me', 'notify_email_opened', 'notify_link_clicked',
+        'created_at'
+    )
+    list_filter = ('owner', ('created_at', DateFieldListFilter))
     date_hierarchy = 'modified_at'
 
 admin.site.register(models.Message, MessageAdmin)
