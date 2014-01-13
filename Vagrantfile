0a09d3756df9b1949fc1faa98f90bdf5b2a02a80
Andrew Smith <andrew.smith@kneto.fi>
2014-01-13 14:12:55 +0200

    update for tomcat and vagrant. plus removed the cursertools and set FitWidthOnLoad to true

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,7 @@
 Vagrant.configure("2") do |config|
 
    config.vm.box = "debain-wheezy"
+   config.vm.synced_folder ".", "/vagrant", :mount_options => ["dmode=777","fmode=666"] 
    config.vm.network "public_network", ip: "192.168.0.133"
    config.vm.provision :shell, :path => "bootstrap.sh"
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,8 @@ apt-get update -y
 apt-get install -y sudo build-essential libjpeg-dev python-dev git-core 
 apt-get install -y postgresql libpq-dev memcached redis-server imagemagick
 apt-get install -y python-setuptools  python-pip
+apt-get install -y tomcat6 tomcat6-admin tomcat6-common 
+apt-get install -y libjpeg-dev libfreetype6-dev libgif-dev swftools
 ln -fs /vagrant /opt/cc
 pip install virtualenv virtualenvwrapper python-memcached
 echo "export WORKON_HOME=\$HOME/.virtualenvs" >> /etc/skel/.bashrc
