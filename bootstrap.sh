463bfd97fd45f02beb2efebeb48655d1bb74f3f9
Andrew Smith <andrew.smith@kneto.fi>
2014-01-22 15:29:26 +0200

    removed tomcat since it is not needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,6 @@ apt-get update -y
 apt-get install -y sudo build-essential libjpeg-dev python-dev git-core 
 apt-get install -y postgresql libpq-dev memcached redis-server imagemagick
 apt-get install -y python-setuptools  python-pip
-apt-get install -y tomcat6 tomcat6-admin tomcat6-common 
 apt-get install -y libjpeg-dev libfreetype6-dev libgif-dev swftools mupdf
 ln -fs /vagrant /opt/cc
 pip install virtualenv virtualenvwrapper python-memcached
