8a8995f79d9bdf3544b79f711d57f6f2224d0342
Andrew Smith <andrew.smith@kneto.fi>
2014-03-05 11:02:35 +0200

    new change subscription page, vat notice and plans with vat.

--- cc/stripe_plans.py
+++ cc/stripe_plans.py
@@ -3,29 +3,33 @@
 PAYMENTS_PLANS = {
     "monthly": {
         "stripe_plan_id": "monthly",
-        "name": "Single User (€149/month)",
+        "name": "Single User",
         "description": "Monthly subscription to Kneto.",
-        "price": 149,
+        "price": 184.76,
         "currency": "eur",
-        "interval": "month"
+        "interval": "month",
+        "interval_count": 1,
+        "metadata": { "VAT" : 35.76 }
     },
     "early_small": {
         "stripe_plan_id": "early_small",
         "name": "Early Access small plan",
         "description": "Early Access for three months, Three users",
-        "price": 449,
+        "price": 556.76,
         "currency": "eur",
         "interval": "month",
         "interval_count": 3,
+        "metadata": { "VAT" : 107.76 }
     },
     "early_large": {
         "stripe_plan_id": "early_large",
         "name": "Early Access large plan",
         "description": "Early Access for three months, Ten users",
-        "price": 1499,
+        "price": 1858.76,
         "currency": "eur",
         "interval": "month",
         "interval_count": 3,
+        "metadata": { "VAT" : 359.76 }
     },
 
 }
