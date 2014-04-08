b09687ec248375bf6c743f8c3921ef5a15051dc7
Andrew Smith <andrew.smith@kneto.fi>
2014-04-08 11:50:07 +0300

    updated stripe plans. I might need to rename them.

--- cc/stripe_plans.py
+++ cc/stripe_plans.py
@@ -7,38 +7,38 @@ PAYMENTS_PLANS = {
         'stripe_plan_id': 'monthly',
         'name': 'Single User',
         'description': 'Monthly subscription to Kneto.',
-        'price': 184.76,
+        'price': 24.78,
         'currency': 'eur',
         'interval': 'month',
         'interval_count': 1,
         'metadata': {
-            'VAT': 35.76,
+            'VAT': 4.79,
             'users': 1
         }
     },
     'early_small': {
         'stripe_plan_id': 'early_small',
         'name': 'Early Access small plan',
-        'description': 'Early Access for three months, Three users',
-        'price': 556.76,
+        'description': 'Early Access, Five users',
+        'price': 186.00,
         'currency': 'eur',
         'interval': 'month',
-        'interval_count': 3,
+        'interval_count': 1,
         'metadata': {
-            'VAT': 107.76,
+            'VAT': 36.00,
             'users': 5
         }
     },
     'early_large': {
         'stripe_plan_id': 'early_large',
         'name': 'Early Access large plan',
-        'description': 'Early Access for three months, Ten users',
-        'price': 1858.76,
+        'description': 'Early Access, Twelve users',
+        'price': 618.76,
         'currency': 'eur',
         'interval': 'month',
-        'interval_count': 3,
+        'interval_count': 1,
         'metadata': {
-            'VAT': 359.76,
+            'VAT': 119.76,
             'users': 12
         }
     },
