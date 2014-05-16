84565a6cfebc5db73d7e1ce4ecfbd928d361a12d
Hieu Nguyen <hieuh25@gmail.com>
2014-05-16 04:35:15 +0300

    Resolve #92: insert video to message

--- cc/static/js/send_message.js
+++ cc/static/js/send_message.js
@@ -28,7 +28,7 @@ $(document).ready(function () {
                 ['fontsize', ['fontsize']],
                 ['color', ['color']],
                 ['para', ['ul', 'ol']],
-                ['insert', ['picture', 'link']],
+                ['insert', ['link', 'picture', 'video']],
                 ['options', ['codeview']],
             ],
             disableDragAndDrop: true,
