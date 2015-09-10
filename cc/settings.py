87e15f95b1c658e471fcbea6bc4ba7751161981a
Kamil Biernat <Kamil.Biernat@rec-global.com>
2015-09-10 09:39:58 +0200

    remove mail section

--- cc/settings.py
+++ cc/settings.py
@@ -362,14 +362,7 @@ try:
     from local_settings import *
 except ImportError:
     print 'local_settings.py doesnt exist yet. Make sure you have created it.'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'test@gmail.com'
-EMAIL_HOST_PASSWORD = 'test'
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'biernyk@gmail.com'
+ 
 # merge FABRIC settings
 FABRIC.update(FABRIC_GLOBAL)
 
